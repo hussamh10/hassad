ebbee8b5fa7d0cf6fe164f6d67677be5a661c432
Usman Rafi <usman.rafi.96@gmail.com>
2018-03-09 20:34:13 +0500

    Added Info.java

--- src/player/Info.java
+++ src/player/Info.java
@@ -1,5 +1,60 @@
 package player;
 
+import java.util.Date;
+
 public class Info {
-    // TODO
+    private String name;
+    private String email;
+    private Date DOB;
+    private String location;
+
+
+    public Info(String name, String email, Date DOB, String location) {
+        this.name = name;
+        this.email = email;
+        this.DOB = DOB;
+        this.location = location;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getDOB() {
+        return DOB;
+    }
+
+    public void setDOB(Date DOB) {
+        this.DOB = DOB;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    @Override
+    public String toString() {
+        return "Info{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", DOB=" + DOB +
+                ", location='" + location + '\'' +
+                '}';
+    }
 }
