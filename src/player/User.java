22860975a51faa93b7b7b7eb576f6ffe6c66179e
hussamh10 <hussamh10@gmail.com>
2018-03-17 14:23:23 +0500

    Added friends UUIDS

--- src/player/User.java
+++ src/player/User.java
@@ -1,5 +1,6 @@
 package player;
 
+import java.util.ArrayList;
 import java.util.UUID;
 
 public class User {
@@ -7,12 +8,14 @@ public class User {
     private int points;
     private int coins;
     private Info info;
+    private ArrayList<UUID> friends_ids;
 
     public User(UUID id, int points, int coins, Info info) {
         this.id = id;
         this.points = points;
         this.coins = coins;
         this.info = info;
+        friends_ids = new ArrayList<UUID>();
     }
 
     public UUID getId() {
@@ -43,7 +46,6 @@ public class User {
 
     public void setInfo(Info info) {this.info = info;}
 
-
     public void addCoins(int amount){
         this.coins += amount;
     }
@@ -55,6 +57,15 @@ public class User {
     public void addPoints(int amount){
         this.points += amount;
     }
+
+    public void addFriend(UUID friend_id){
+        friends_ids.add(friend_id);
+    }
+
+    public ArrayList<UUID> getFriends(){
+        return friends_ids;
+    }
+
 
     @Override
     public String toString() {
