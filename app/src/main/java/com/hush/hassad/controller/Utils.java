d7c7690c0c3815f33f792e3eb2744bf50eef3c6b
hussamh10 <hussamh10@gmail.com>
2018-03-31 18:20:29 +0500

    Added util

---
+++ app/src/main/java/com/hush/hassad/controller/Utils.java
@@ -0,0 +1,18 @@
+package com.hush.hassad.controller;
+
+import java.util.Date;
+
+/**
+ * Created by hussam on 3/31/18.
+ */
+
+public class Utils {
+	Utils(){
+
+	}
+
+	public static String getTimeString(Date date){
+		// FIXME
+		return "11:45 pm";
+	}
+}
