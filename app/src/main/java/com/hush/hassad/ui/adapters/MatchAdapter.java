479a695433332b6fbacfc75d303fce0c2360465e
Mohammad Saad Mujeeb <saadmujeeb05@gmail.com>
2018-03-31 17:57:38 +0500

    Match adapter

---
+++ app/src/main/java/com/hush/hassad/ui/adapters/MatchAdapter.java
@@ -0,0 +1,84 @@
+package com.hush.hassad.ui.adapters;
+
+import android.app.Activity;
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.hush.hassad.R;
+import com.hush.hassad.controller.competition.Match;
+import java.util.ArrayList;
+
+/**
+ * Created by Saad Mujeeb on 31/3/2018.
+ */
+
+public class MatchAdapter extends ArrayAdapter {
+
+	Activity activity;
+	int resource;
+	ArrayList<Match> matches;
+	MatchHolder matchHolder;
+
+	static class MatchHolder{
+		ImageView home_team_img;
+		TextView home_team_name;
+		TextView pred_home_score;
+		TextView home_score;
+
+		ImageView away_team_img;
+		TextView away_team_name;
+		TextView pred_away_score;
+		TextView away_score;
+
+		TextView match_time;
+	}
+
+
+	public MatchAdapter(Activity activity, int resource, ArrayList<Match> matches, ArrayList<Long> quantities){
+		super(activity,resource,matches);
+		this.activity = activity;
+		this.matches = matches;
+		this.resource = resource;
+	}
+
+
+	@NonNull
+	@Override
+	public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+		if (convertView == null) {
+			convertView = activity.getLayoutInflater().inflate(resource, parent, false);
+		}
+
+		matchHolder.home_team_img = (ImageView) convertView.findViewById(R.id.home_team_img);
+		matchHolder.home_team_name = (TextView) convertView.findViewById(R.id.home_team_name);
+		matchHolder.pred_home_score = (TextView) convertView.findViewById(R.id.pred_home_score);
+		matchHolder.home_score = (TextView) convertView.findViewById(R.id.home_score);
+
+		matchHolder.away_team_img = (ImageView) convertView.findViewById(R.id.away_team_img);
+		matchHolder.away_team_name = (TextView) convertView.findViewById(R.id.away_team_name);
+		matchHolder.pred_away_score = (TextView) convertView.findViewById(R.id.pred_away_score);
+		matchHolder.away_score = (TextView) convertView.findViewById(R.id.away_score);
+
+		matchHolder.match_time = (TextView) convertView.findViewById(R.id.match_time);
+
+
+		Match match = matches.get(position);
+		matchHolder.home_team_img.setImageResource(R.drawable.manchester_united);
+		matchHolder.home_team_name.setText(match.getHome().getName().toString());
+
+		matchHolder.away_team_img.setImageResource(R.drawable.chelsea);
+		matchHolder.away_team_name.setText(match.getAway().getName().toString());
+
+		if(!match.isEnded())
+			matchHolder.match_time.setText(match.getKickoff_time().toString());
+
+		return convertView;
+	}
+}
--- app/src/main/java/com/hush/hassad/ui/fragments/DayFragment.java
+++ app/src/main/java/com/hush/hassad/ui/fragments/DayFragment.java
@@ -6,11 +6,18 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.hush.hassad.R;
+import com.hush.hassad.controller.competition.Match;
+import com.hush.hassad.ui.adapters.MatchAdapter;
+
+import java.util.ArrayList;
 
 public class DayFragment extends Fragment {
 
+	ArrayList<Match> matches;
+	MatchAdapter matchAdapter;
 
 	public DayFragment() {
 
@@ -19,6 +26,10 @@ public class DayFragment extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 							 Bundle savedInstanceState) {
-		return inflater.inflate(R.layout.fragment_home_sub, container, false);
+		View view = inflater.inflate(R.layout.fragment_home_sub, container, false);
+		ListView matchList = (ListView) view.findViewById(R.id.match_list);
+		matchAdapter = new MatchAdapter(getActivity(), R.layout.card_match, matches ,null);
+		matchList.setAdapter(matchAdapter);
+		return view;
 	}
 }
