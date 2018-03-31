8121facbc57cb4c3b72e73a7b690c1a91585eed5
hussamh10 <hussamh10@gmail.com>
2018-03-31 16:01:12 +0500

    Merged

---
+++ app/src/main/java/com/hush/hassad/controller/predictions/TournamentPrediction.java
@@ -0,0 +1,26 @@
+package com.hush.hassad.controller.predictions;
+
+import com.hush.hassad.controller.competition.results.TournamentResult;
+import com.hush.hassad.controller.player.User;
+
+import java.util.UUID;
+
+public class TournamentPrediction extends Prediction{
+    private TournamentResult predicted_result;
+
+    public TournamentPrediction(UUID id, TournamentResult predicted_result, User predictor){
+        super(id, predictor);
+        this.predicted_result = predicted_result;
+    }
+
+    public TournamentResult getPredicted_result() {
+        return predicted_result;
+    }
+
+    @Override
+    public String toString() {
+        return "MatchPrediction{" +
+                "predicted_result=" + predicted_result +
+                '}' + super.toString();
+    }
+}
