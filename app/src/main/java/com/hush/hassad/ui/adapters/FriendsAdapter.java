190767c3a391f8066860eb9911f760bbcafdd335
Usman Rafi <usman.rafi.96@gmail.com>
2018-04-07 19:00:00 +0500

    Modified FriendsFragment

---
+++ app/src/main/java/com/hush/hassad/ui/adapters/FriendsAdapter.java
@@ -0,0 +1,59 @@
+package com.hush.hassad.ui.adapters;
+
+import android.app.Activity;
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.hush.hassad.R;
+import com.hush.hassad.controller.player.User;
+
+import java.util.ArrayList;
+
+public class FriendsAdapter extends ArrayAdapter {
+    Activity activity;
+    int resource;
+    ArrayList<User> friends;
+
+    FriendHolder mFriendHolder;
+
+    static class FriendHolder{
+        ImageView friend_image;
+        TextView friend_name;
+    }
+
+
+    public FriendsAdapter(Activity activity, int resource, ArrayList<User> friends) {
+        super(activity, resource);
+        this.activity = activity;
+        this.resource = resource;
+        this.friends = friends;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        if (convertView == null) {
+            convertView = activity.getLayoutInflater().inflate(resource, parent, false);
+        }
+
+        mFriendHolder = new FriendHolder();
+
+        mFriendHolder.friend_name = convertView.findViewById(R.id.friend_name);
+        mFriendHolder.friend_image = convertView.findViewById(R.id.friend_img);
+
+        User friend = friends.get(position);
+
+        mFriendHolder.friend_name.setText(friend.getInfo().getName());
+        mFriendHolder.friend_image.setImageResource(R.drawable.profile);
+
+        return convertView;
+    }
+
+}
--- app/src/main/java/com/hush/hassad/ui/fragments/FriendsFragment.java
+++ app/src/main/java/com/hush/hassad/ui/fragments/FriendsFragment.java
@@ -7,24 +7,32 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.hush.hassad.R;
+import com.hush.hassad.controller.player.User;
+import com.hush.hassad.ui.adapters.FriendsAdapter;
+
+import java.util.ArrayList;
 
 public class FriendsFragment extends Fragment {
+
+	ListView friendsList;
+	FriendsAdapter mFriendsAdapter;
+
+	ArrayList<User> friends;
 
 	@Nullable
 	@Override
 	public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.fragment_friends, container,false);
 
-		FloatingActionButton fab = view.findViewById(R.id.fab);
-		fab.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View view) {
-				Toast.makeText(getActivity(), "New friend", Toast.LENGTH_SHORT).show();
-			}
-		});
+		friendsList = view.findViewById(R.id.friends_list);
+
+		mFriendsAdapter = new FriendsAdapter(getActivity(), R.layout.card_friend, friends);
+
+		friendsList.setAdapter(mFriendsAdapter);
 
 		return view;
 	}
