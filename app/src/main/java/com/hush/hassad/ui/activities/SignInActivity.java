ef9dbffccc060e16b9ef17dd4ced575ae820bd33
Muhammad Haroon <m_haroon96@hotmail.com>
2018-06-13 00:34:39 +0500

    removed unusued boilerplate code

--- app/src/main/java/com/hush/hassad/ui/activities/SignInActivity.java
+++ app/src/main/java/com/hush/hassad/ui/activities/SignInActivity.java
@@ -1,17 +1,10 @@
 package com.hush.hassad.ui.activities;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.os.Handler;
-import android.util.Log;
-import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
@@ -28,106 +21,22 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
-import com.google.firebase.auth.ProviderQueryResult;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.hush.hassad.R;
 import com.hush.hassad.controller.Manager;
-import com.hush.hassad.controller.player.Info;
 import com.hush.hassad.controller.player.User;
 import com.hush.hassad.dal.DAL;
 
-/**
- * An example full-screen activity that shows and hides the system UI (i.e.
- * status bar and navigation/system bar) with user interaction.
- */
 public class SignInActivity extends AppCompatActivity {
-	/**
-	 * Whether or not the system UI should be auto-hidden after
-	 * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
-	 */
-	private static final boolean AUTO_HIDE = true;
-
+	
 	private GoogleSignInClient mGoogleSignInClient;
 	private FirebaseAuth mAuth;
 	private SignInButton btn_signin;
-	private boolean exists;
 
 	private static final int NEW_USER = 0;
 	private static final int OLD_USER = 1;
-
-
-	/**
-	 * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
-	 * user interaction before hiding the system UI.
-	 */
-	private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
-
-	/**
-	 * Some older devices needs a small delay between UI widget updates
-	 * and a change of the status and navigation bar.
-	 */
-	private static final int UI_ANIMATION_DELAY = 300;
-	private final Handler mHideHandler = new Handler();
-	private View mContentView;
-	private final Runnable mHidePart2Runnable = new Runnable() {
-		@SuppressLint("InlinedApi")
-		@Override
-		public void run() {
-			// Delayed removal of status and navigation bar
-
-			// Note that some of these constants are new as of API 16 (Jelly Bean)
-			// and API 19 (KitKat). It is safe to use them, as they are inlined
-			// at compile-time and do nothing on earlier devices.
-			mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-					| View.SYSTEM_UI_FLAG_FULLSCREEN
-					| View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-					| View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-					| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-					| View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
-		}
-	};
-	private View mControlsView;
-	private final Runnable mShowPart2Runnable = new Runnable() {
-		@Override
-		public void run() {
-			// Delayed display of UI elements
-			ActionBar actionBar = getSupportActionBar();
-			if (actionBar != null) {
-				actionBar.show();
-			}
-			mControlsView.setVisibility(View.VISIBLE);
-		}
-	};
-	private boolean mVisible;
-	private final Runnable mHideRunnable = new Runnable() {
-		@Override
-		public void run() {
-			hide();
-		}
-	};
-	/**
-	 * Touch listener to use for in-layout UI controls to delay hiding the
-	 * system UI. This is to prevent the jarring behavior of controls going away
-	 * while interacting with activity UI.
-	 */
-	private final View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
-		@Override
-		public boolean onTouch(View view, MotionEvent motionEvent) {
-			if (AUTO_HIDE) {
-				delayedHide(AUTO_HIDE_DELAY_MILLIS);
-			}
-			return false;
-		}
-	};
 
 	@Override
 	public void onStart() {
@@ -238,110 +147,22 @@ public class SignInActivity extends AppCompatActivity {
 	}
 
 	public void checkExistence(String uid) {
-
+		
 		FirebaseFirestore db = FirebaseFirestore.getInstance();
 		Query query = db.collection("users").whereEqualTo("id", uid);
-
+		
 		query.get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
 			@Override
 			public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
 				FirebaseUser user = mAuth.getCurrentUser();
-				if(queryDocumentSnapshots.isEmpty()){
+				if (queryDocumentSnapshots.isEmpty()) {
 					//The user exists...
 					update(user, NEW_USER);
-				}
-				else {
+				} else {
 					//The user doesn't exist...
 					update(user, OLD_USER);
 				}
 			}
 		});
-		/*final String id = uid;
-		DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
-		DatabaseReference userNameRef = rootRef.child("users");
-		ValueEventListener eventListener = new ValueEventListener() {
-			@Override
-			public void onDataChange(DataSnapshot dataSnapshot) {
-				FirebaseUser user = mAuth.getCurrentUser();
-				for(DataSnapshot data: dataSnapshot.getChildren()){
-					if (data.child(id).exists()) {
-						update(user, OLD_USER);
-					} else {
-						update(user, NEW_USER);
-					}
-				}*/
-				/*if(dataSnapshot.exists()){
-					update(user, OLD_USER);
-				} else {
-					update(user, NEW_USER);
-				}
-			}
-
-			@Override
-			public void onCancelled(DatabaseError databaseError) {}
-		};
-		userNameRef.addListenerForSingleValueEvent(eventListener);
-
-		/*FirebaseDatabase.getInstance().getReference(uid).addValueEventListener(new ValueEventListener() {
-			@Override
-			public void onDataChange(DataSnapshot dataSnapshot) {
-				FirebaseUser user = mAuth.getCurrentUser();
-				if(dataSnapshot.exists()){
-					update(user, NEW_USER);
-				} else {
-					update(user, OLD_USER);
-				}
-			}
-
-			@Override
-			public void onCancelled(DatabaseError databaseError) {
-
-			}
-		});*/
-	}
-
-/*	@Override
-	protected void onPostCreate(Bundle savedInstanceState) {
-		super.onPostCreate(savedInstanceState);
-
-		// Trigger the initial hide() shortly after the activity has been
-		// created, to briefly hint to the user that UI controls
-		// are available.
-		delayedHide(100);
-	}*/
-
-	private void hide() {
-		// Hide UI first
-		ActionBar actionBar = getSupportActionBar();
-		if (actionBar != null) {
-			actionBar.hide();
-		}
-		mControlsView.setVisibility(View.GONE);
-		mVisible = false;
-
-		// Schedule a runnable to remove the status and navigation bar after a delay
-		mHideHandler.removeCallbacks(mShowPart2Runnable);
-		mHideHandler.postDelayed(mHidePart2Runnable, UI_ANIMATION_DELAY);
-	}
-
-	@SuppressLint("InlinedApi")
-	private void show() {
-		// Show the system bar
-		mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-				| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
-		mVisible = true;
-
-		// Schedule a runnable to display UI elements after a delay
-		mHideHandler.removeCallbacks(mHidePart2Runnable);
-		mHideHandler.postDelayed(mShowPart2Runnable, UI_ANIMATION_DELAY);
-	}
-
-	/**
-	 * Schedules a call to hide() in delay milliseconds, canceling any
-	 * previously scheduled calls.
-	 */
-	private void delayedHide(int delayMillis) {
-		mHideHandler.removeCallbacks(mHideRunnable);
-		mHideHandler.postDelayed(mHideRunnable, delayMillis);
 	}
 }
