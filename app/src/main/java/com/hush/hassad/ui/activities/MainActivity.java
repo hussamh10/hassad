283b9cba99434a12cebb3d4c6208567fb706b240
Muhammad Haroon <m_haroon96@hotmail.com>
2018-06-24 15:43:52 +0500

    nav bar styles

--- app/src/main/java/com/hush/hassad/ui/activities/MainActivity.java
+++ app/src/main/java/com/hush/hassad/ui/activities/MainActivity.java
@@ -130,8 +130,10 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
+		
+        toolbar.setTitleTextColor(getResources().getColor(R.color.wc_gold));
+		setSupportActionBar(toolbar);
+        
 		loadProfile();
 
 		boolean prediction_made = getIntent().getBooleanExtra("prediction", false);
