e0d339a444c7999ea17bcb4b21d432bef0dc2080
Muhammad Haroon <m_haroon96@hotmail.com>
2018-06-27 21:28:10 +0500

    added getTeamFlagResource method

--- app/src/main/java/com/hush/hassad/controller/Utils.java
+++ app/src/main/java/com/hush/hassad/controller/Utils.java
@@ -1,5 +1,7 @@
 package com.hush.hassad.controller;
 
+import com.hush.hassad.R;
+
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
@@ -37,5 +39,28 @@ public class Utils {
 		boolean sameDay = calendar1.get(Calendar.DAY_OF_MONTH) == calendar2.get(Calendar.DAY_OF_MONTH);
 		return (sameDay && sameMonth && sameYear);
 		
+	}
+	
+	public static int getTeamFlagResource(int id) {
+		switch (id) {
+			// TODO: set appropriate returns
+			case 1: return R.drawable.belgium;
+			case 2: return R.drawable.belgium;
+			case 3: return R.drawable.belgium;
+			case 4: return R.drawable.belgium;
+			case 5: return R.drawable.belgium;
+			case 6: return R.drawable.belgium;
+			case 7: return R.drawable.belgium;
+			case 8: return R.drawable.belgium;
+			case 9: return R.drawable.belgium;
+			case 10: return R.drawable.belgium;
+			case 11: return R.drawable.belgium;
+			case 12: return R.drawable.belgium;
+			case 13: return R.drawable.belgium;
+			case 14: return R.drawable.belgium;
+			case 15: return R.drawable.belgium;
+			case 16: return R.drawable.belgium;
+		}
+		return R.drawable.app_logo;
 	}
 }
